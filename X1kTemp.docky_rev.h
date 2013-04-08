72e024e47485ecd400bdf264850272764100d341
abalabanb <>
2013-04-08 22:09:07 +0000

    - UPD: version number bumped to 53.2

--- X1kTemp.docky_rev.h
+++ X1kTemp.docky_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION    1
-#define DATE		"20.03.2013"
-#define VERS		"X1ktemp.docky 53.1"
-#define VSTRING		"X1ktemp.docky 53.1 (20.03.2013)\r\n"
-#define VERSTAG		"\0$VER: X1ktemp.docky 53.1 (20.03.2013)"
+#define REVISION	2
+#define DATE		"7.4.2013"
+#define VERS		"X1kTemp.docky 53.2"
+#define VSTRING		"X1kTemp.docky 53.2 (7.4.2013)\r\n"
+#define VERSTAG		"\0$VER: X1kTemp.docky 53.2 (7.4.2013)"
