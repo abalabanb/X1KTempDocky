044000c7c5775111a7440d41595131d289753730
abalabanb <>
2013-04-13 21:50:46 +0000

    - UPD: date

--- X1kTemp.docky_rev.h
+++ X1kTemp.docky_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
 #define REVISION	4
-#define DATE		"9.4.2013"
+#define DATE		"13.4.2013"
 #define VERS		"X1kTemp.docky 53.4"
-#define VSTRING		"X1kTemp.docky 53.4 (9.4.2013)\r\n"
-#define VERSTAG		"\0$VER: X1kTemp.docky 53.4 (9.4.2013)"
+#define VSTRING		"X1kTemp.docky 53.4 (13.4.2013)\r\n"
+#define VERSTAG		"\0$VER: X1kTemp.docky 53.4 (13.4.2013)"
