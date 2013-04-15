811ff0aecbd5cdf5a90bd6fc9a82ee5a7e442168
abalabanb <>
2013-04-15 21:50:40 +0000

    - UPD: bumping revision to 53.5

--- X1kTemp.docky_rev.h
+++ X1kTemp.docky_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	4
-#define DATE		"13.4.2013"
-#define VERS		"X1kTemp.docky 53.4"
-#define VSTRING		"X1kTemp.docky 53.4 (13.4.2013)\r\n"
-#define VERSTAG		"\0$VER: X1kTemp.docky 53.4 (13.4.2013)"
+#define REVISION	5
+#define DATE		"15.4.2013"
+#define VERS		"X1kTemp.docky 53.5"
+#define VSTRING		"X1kTemp.docky 53.5 (15.4.2013)\r\n"
+#define VERSTAG		"\0$VER: X1kTemp.docky 53.5 (15.4.2013)"
