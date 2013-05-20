0df217fc605870e899fc5fed9161d733ea3b4098
abalabanb <>
2013-05-20 12:21:22 +0000

    - UPD: revbumped to 53.8

--- X1kTemp.docky_rev.h
+++ X1kTemp.docky_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	7
-#define DATE		"23.4.2013"
-#define VERS		"X1kTemp.docky 53.7"
-#define VSTRING		"X1kTemp.docky 53.7 (23.4.2013)\r\n"
-#define VERSTAG		"\0$VER: X1kTemp.docky 53.7 (23.4.2013)"
+#define REVISION	8
+#define DATE		"16.5.2013"
+#define VERS		"X1kTemp.docky 53.8"
+#define VSTRING		"X1kTemp.docky 53.8 (16.5.2013)\r\n"
+#define VERSTAG		"\0$VER: X1kTemp.docky 53.8 (16.5.2013)"
